beea626adef6d78a5911afd37d7c010cf2e1d9f7
Nelson Kimutai Cheruiyot <lsoncheruiyot@gmail.com>
2025-06-20 18:12:34 +0600

    changes to misplaced function calls

--- script.js
+++ script.js
@@ -71,28 +71,28 @@
 
 let intervalID;
 
-function colorChange() {
+function startChange() {
     if (!intervalID) {
-        intervalID = setInterval(changeRandomColor, 1000);
+        intervalID = setInterval(colorChange, 1000);
     }
     
 }
 
-// function startChange() {
-//     if (document.body.style.backgroundColor !=='black') {
-//         document.body.style.backgroundColor = 'black';
-//         // document.body.style.color = 'white';
-//     } else {
-//         document.body.style.backgroundColor = 'white';
-//         // document.body.style.color = 'black';
-//     }
-// }
-
-
-function changeRandomColor() {
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    document.body.style.backgroundColor = `#${randomColor}`;
+function colorChange() {
+    if (document.body.style.backgroundColor !=='black') {
+        document.body.style.backgroundColor = 'black';
+        // document.body.style.color = 'white';
+    } else {
+        document.body.style.backgroundColor = 'white';
+        // document.body.style.color = 'black';
+    }
 }
+
+
+// function changeRandomColor() {
+//     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+//     document.body.style.backgroundColor = `#${randomColor}`;
+// }
 
 function stopChange(){
     clearInterval(intervalID);
