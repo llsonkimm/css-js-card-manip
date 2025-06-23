c591f92387e83406d212a853d33a82261ebe246b
Nelson Kimutai Cheruiyot <lsoncheruiyot@gmail.com>
2025-06-23 21:15:51 +0600

    logging the returned data to the console

--- script.js
+++ script.js
@@ -189,6 +189,8 @@
 async function getUsers() {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await res.json();
+
+    console.log(data);
 };
 
 getUsers();
